05eb18cbb34da8cc6595febf0216270ef33b6629
Dibora D <diborad@yahoo.com>
2019-09-06 19:25:38 -0500

    Cleanup, rating, image click event

--- assets/javascript/apps.js
+++ assets/javascript/apps.js
@@ -3,31 +3,24 @@ window.onload = function() {
 };
 function registerGifClickEvent() {
   $(".gif").on("click", function() {
-    var person = $(this).attr("data-name");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + person + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
-
+    var title = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?rating=pg&q=" + title + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10"; //10 results at a time
     $.ajax({
       url: queryURL,
       method: "GET"
-    })
-    .then(function(response) {
+    }).then(function(response) {
       var results = response.data;
-
+      console.log(results.length);
       for (var i = 0; i < results.length; i++) {
-        if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
-          var gifDiv = $("<div>");
-
-          var rating = results[i].rating;
-
-          var p = $("<p>").text("Rating: " + rating);
-
-          var personImage = $("<img>");
-
-          personImage.attr("src", results[i].images.fixed_height.url).attr("class", "Image");
-          gifDiv.append(p);
-          gifDiv.append(personImage);
-          $("#gifs-appear-here").prepend(gifDiv);
-        }
+        var gifDiv = $("<div>");
+        var rating = results[i].rating;
+        var p = $("<p>").text("Rating: " + rating);
+        var resultImage = $("<img>");
+        resultImage.attr("src", results[i].images.fixed_height.url).attr("class", "resultImage");
+        gifDiv.append(p);
+        gifDiv.append(resultImage);
+        $("#gifs-appear-here").prepend(gifDiv);
+        registerImageClickEvent();
       }
     });
   });
@@ -55,3 +48,18 @@ $("#add-giphy").on("click", function(event) {
   gifs.push(text);
   renderGiphyButtons();
 });
+function registerImageClickEvent() {
+  $(".resultImage").on("click", function() {
+    var state = $(this).attr("data-state");
+    // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+    // Then, set the image's data-state to animate
+    // Else set src to the data-still value
+    if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    } else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
+  });
+}
